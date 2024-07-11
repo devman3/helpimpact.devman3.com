b91fd420bc77fa5dbb2a740399ed8dba2afda313
Carolann9 <carol@devman3.com>
2024-07-11 10:13:26 +0200

    2024/07/11 10:13

--- jspopups/gsydashboard.js
+++
@@ -1,4 +0,0 @@
-hmLoadPopup({
-hmTitle:"Dashboard",
-hmBody:"<p class=\"p_KidzNormal\"><span class=\"f_KidzNormal\" style=\"font-weight: bold;\">Centralise vital information<\/span><span class=\"f_KidzNormal\"> - Critical data is brought to the surface. Instead of flipping through pages, everyone will be on the same page.<\/span><\/p>\n\r<p class=\"p_KidzNormal\"><span class=\"f_KidzNormal\" style=\"font-weight: bold;\">Data visualization<\/span><span class=\"f_KidzNormal\"> - The visual representation helps you interpret your data and track performance metrics, enabling intuitive decision making. <\/span><\/p>\n\r"
-})
