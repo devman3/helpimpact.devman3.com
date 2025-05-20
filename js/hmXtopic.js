fdd4d867b82bea9eb533f2798e992fd836c31a06
Carol Ann Sauls <carol@devman3.com>
2025-05-20 11:26:23 +0200

    2025/05/20 11:26

--- js/hmXtopic.js
+++ js/hmXtopic.js
@@ -6,7 +6,7 @@ hmProjectInfo = {
 	copyright: "© 2025 IS Kidz Africa Pty Ltd",
 	summary: "Proud authors of DevMan bursary, fundraising and grant making web application",
 	version: "1.0.0",
-	date: "Friday, 09 May 2025",
+	date: "Tuesday, 20 May 2025",
 	language: "en-za",
 	mainfile: "index.html"
 	}
